250213b917dd8fb3e0e070cfc631ecac98f18176
David Heward <dave@bytewire.co.uk>
2013-02-22 09:56:56 +0000

    adding in the required

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+$dbh = new PDO('mysql:host=xserve1.dc.fubra.net;dbname=bytewire-bytewire', 'bytewire-zjav', 'HhHNKzelzJrF08DH');
+
+print_r($dbh);
+
+?>
+
---
+++ library/oneall.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+* Oneall Actions
+*/
+class Oneall_Actions
+{
+	
+	function __construct()
+	{
+		// do something
+	}
+
+	function GetUserIDForUserToken(){
+
+	}
+
+	function GetUserTokenForUserID(){
+
+	}
+
+	function LinkUserTokenToID(){
+
+	}
+
+	function UnlinkUserTokenFromUserID(){
+		
+	}
+
+}
+
+?>
