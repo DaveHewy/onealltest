ae2177c4b62b72d2f0a64e8d5d86439381c1b7fb
David Heward <dave@bytewire.co.uk>
2013-02-22 10:15:08 +0000

    adding in the required

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 
 $dbh = new PDO('mysql:host=xserve1.dc.fubra.net;dbname=bytewire-bytewire', 'bytewire-zjav', 'HhHNKzelzJrF08DH');
 $dbh->setFetchMode(PDO::FETCH_OBJ);
-foreach($dbh->query("SELECT * FROM user_social_link") as $row)
-{
-	print_r($row);
-}
+$row = $dbh->query("SELECT * FROM user_social_link")->fetchObject();]
+
+print_r($row);
 
 ?>
 
