6cb27c463727c9366b380ac42e3b521515befbfc
David Heward <dave@bytewire.co.uk>
2013-02-22 10:37:22 +0000

    add more generally

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $dbh = new PDO('mysql:host=xserve1.dc.fubra.net;dbname=bytewire-bytewire', 'bytewire-zjav', 'HhHNKzelzJrF08DH');
 $dbh->setFetchMode(PDO::FETCH_OBJ);
-$row = $dbh->query("SELECT * FROM user_social_link")->fetchObject();
+$row = $dbh->query("SELECT * FROM user_social_link WHERE id='1'")->fetchObject();
 print_r($row);
 
 
